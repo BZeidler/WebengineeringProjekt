5d6fe578c3682dfa8abb9315e05f91272426e57c
Bernhard Zeidler <b.zeidler@yahoo.de>
2017-08-21 14:07:51 +0200

    Suchleiste& Button entfernt, Login Button eingefügt

--- src/main/frontend/fratcher/src/components/Navigation.js
+++ src/main/frontend/fratcher/src/components/Navigation.js
@@ -10,9 +10,7 @@ class Navigation extends React.Component {
       <div className="bd-example" data-example-id="">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
@@ -25,15 +23,22 @@ class Navigation extends React.Component {
                 <a className="nav-link disabled" href="#">Disabled</a>
               </li>
             </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/ >
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </div>
-          </nav>
-        </div>
+
+            <a className="navbar-nav Login" href="#">Login</a>
+          </div>
+
+        </nav>
+      </div>
       );
     }
   }
-
+  //
+  //z13 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
+  //    <span className="navbar-toggler-icon"></span>
+  // </button>
+  //
+  //z26 <form className="form-inline my-2 my-lg-0">
+  //   <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/ >
+  //      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  // </form>
   export default Navigation;
