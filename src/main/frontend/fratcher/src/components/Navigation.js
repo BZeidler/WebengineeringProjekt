5fb66a953e364890df82c80e0062b4644038ab9b
Bernhard Zeidler <b.zeidler@yahoo.de>
2017-08-23 10:20:04 +0200

    Nav Liste hat nun "ListMatches", überflüssigen "Button" entfernt

--- src/main/frontend/fratcher/src/components/Navigation.js
+++ src/main/frontend/fratcher/src/components/Navigation.js
@@ -13,13 +13,10 @@ class Navigation extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/api/matching">Matches<span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
+                <a className="nav-link" href="/api/matching">Find People</a>
               </li>
             </ul>
               <ul className="nav navbar-nav navbar-right">
